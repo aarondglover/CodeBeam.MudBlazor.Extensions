bbcc1766da8d17a0c7611cf3a8e0dd252742d9e9
Aaron Glover <aarondglover@users.noreply.github.com>
2026-09-15 23:45:37 +1000

    Dispose benchmark contexts asynchronously

--- benchmarks/CodeBeam.MudBlazor.Extensions.Benchmarks/SelectInitialRenderBenchmarks.cs
+++ benchmarks/CodeBeam.MudBlazor.Extensions.Benchmarks/SelectInitialRenderBenchmarks.cs
@@ -24,10 +24,10 @@ public class SelectInitialRenderBenchmarks
     }
 
     [Benchmark]
-    public int RenderSelect()
+    public async Task<int> RenderSelect()
     {
-        using var context = BenchmarkBunitContext.Create();
-        using var cut = context.Render<SelectBenchmarkHost>(parameters => parameters
+        await using var context = BenchmarkBunitContext.Create();
+        var cut = context.Render<SelectBenchmarkHost>(parameters => parameters
             .Add(x => x.Items, _items)
             .Add(x => x.SelectedValues, _selectedValues)
             .Add(x => x.Virtualize, Virtualize));
