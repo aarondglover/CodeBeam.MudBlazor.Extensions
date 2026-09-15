89d94c67b0ad5a5fba242f6b4b1aff5647232236
Aaron Glover <aarondglover@users.noreply.github.com>
2026-09-15 23:01:13 +1000

    Add strict virtualization regression coverage

---
+++ tests/CodeBeam.MudBlazor.Extensions.UnitTests/Components/SelectExtendedVirtualizationStrictTests.cs
@@ -0,0 +1,43 @@
+using AwesomeAssertions;
+using Bunit;
+using MudExtensions.UnitTests.Extensions;
+
+namespace MudExtensions.UnitTests.Components
+{
+    [TestFixture]
+    public class SelectExtendedVirtualizationStrictTests : BunitTest
+    {
+        [Test]
+        public void StrictMode_ValidOffscreenInitialValue_RemainsRepresentable()
+        {
+            var items = Enumerable.Range(1, 4_000).Select(value => (int?)value).ToList();
+
+            var cut = Context.Render<MudSelectExtended<int?>>(parameters => parameters
+                .Add(x => x.ItemCollection, items)
+                .Add(x => x.Virtualize, true)
+                .Add(x => x.Strict, true)
+                .Add(x => x.Value, 3_999));
+
+            cut.WaitForAssertion(() =>
+                cut.Instance.GetState(x => x.Text).Should().Be("3999"));
+
+            var shadowList = cut.Find("div[style='display: none']");
+            shadowList.QuerySelectorAll("div.mud-list-item-extended").Count().Should().Be(1);
+        }
+
+        [Test]
+        public void StrictMode_ValueOutsideItemCollection_IsNotAddedToShadowList()
+        {
+            var items = Enumerable.Range(1, 100).Select(value => (int?)value).ToList();
+
+            var cut = Context.Render<MudSelectExtended<int?>>(parameters => parameters
+                .Add(x => x.ItemCollection, items)
+                .Add(x => x.Virtualize, true)
+                .Add(x => x.Strict, true)
+                .Add(x => x.Value, 999));
+
+            var shadowList = cut.Find("div[style='display: none']");
+            shadowList.QuerySelectorAll("div.mud-list-item-extended").Should().BeEmpty();
+        }
+    }
+}
